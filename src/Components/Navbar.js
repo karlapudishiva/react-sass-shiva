c949be18494ee6b9d433d66150f38b570b304df1
sai teja <saiteja2511@gmail.com>
2022-07-29 11:47:06 +0530

    react-sass

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        <a href="/profile">profile</a>
+    </div>
+  )
+}
+
+export default Navbar
